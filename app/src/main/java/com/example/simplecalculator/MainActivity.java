c4e2b7f24bf85c57f7cbf0883ecbe735769b086e
Ben Lee <bendelonlee@gmail.com>
2020-07-26 14:03:18 -0600

    Apply fonts. Also, adds imports (which fixes build errors)

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -2,14 +2,41 @@ package com.example.simplecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import android.widget.TextView;
+import android.graphics.Typeface;
+
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare elements
+    TextInputEditText edtNumber1;
+    TextInputEditText edtNumber2;
+    TextView txtResult;
+    Typeface monofettFont;
+    Typeface tekoFont;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtNumber1 = (TextInputEditText) findViewById(R.id.edtNumber1);
+        edtNumber2 = (TextInputEditText) findViewById(R.id.edtNumber2);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        monofettFont = Typeface.createFromAsset(getAssets(), "fonts/Monofett-Regular.ttf");
+        tekoFont = Typeface.createFromAsset(getAssets(), "fonts/Teko-Regular.ttf");
+
+        //Apply Fonts
+
+        edtNumber1.setTypeface(tekoFont);
+        edtNumber2.setTypeface(tekoFont);
+        txtResult.setTypeface(monofettFont);
+
     }
 }
 
